46996b9dd4a71fb2aadbf3fb3b68fb4101d29359
10011999 <motewaraditya77@gmail.com>
2022-09-12 12:27:22 +0530

    Added Firs Player Start Position Zero

---
+++ src/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        int playerPosition = 0;
+        System.out.println("Start a Player position Zero");
+    }
+}
