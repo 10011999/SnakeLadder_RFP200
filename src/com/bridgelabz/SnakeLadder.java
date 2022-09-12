48562f52c7b9e83de090cefc5580546df71ec692
10011999 <motewaraditya77@gmail.com>
2022-09-12 15:14:41 +0530

    Added Check Snake Ladder Position

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -3,7 +3,25 @@ package com.bridgelabz;
 public class SnakeLadder {
     public static void main(String[] args) {
         int playerPosition = 0;
-        int afterRolling = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println("After Rolling Position = " +afterRolling );
+        int playerPlay = (int) Math.floor(Math.random() *10) % 2;
+        final int snake = 1;
+        final int noplay = 2;
+        final int ladder = 3;
+        int options = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        switch (options) {
+            case ladder:
+                playerPosition += playerPlay;
+                System.out.println("the player got Ladder:" + playerPosition);
+                break;
+            case noplay:
+                System.out.println("the player remains in same position:" + playerPosition);
+                break;
+            case snake:
+                playerPosition -= playerPlay;
+                System.out.println("the snake bit the player:" + playerPosition);
+                break;
+        }
+        System.out.println("Player position is :" + playerPosition);
     }
 }
+
