9b6488c4e21c681afa2f45bc5d293fe1f3678839
10011999 <motewaraditya77@gmail.com>
2022-09-12 13:02:55 +0530

    Added Player Rolling Die

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 public class SnakeLadder {
     public static void main(String[] args) {
         int playerPosition = 0;
-        System.out.println("Start a Player position Zero");
+        int afterRolling = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println("After Rolling Position = " +afterRolling );
     }
 }
